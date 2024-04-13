3b5d842ee096db279c05c694481008d85799e17b
claude_uceda <claude.uceda@gmail.com>
2024-04-13 15:18:54 +0200

    Adding failing tests

--- Webatrio.Employee.Services.Test/PersonServiceTests.cs
+++ Webatrio.Employee.Services.Test/PersonServiceTests.cs
@@ -17,5 +17,25 @@ namespace Webatrio.Employee.Services.Test
             Assert.IsTrue(result.Successful);
             Assert.AreNotEqual(Guid.Empty, result.Value!.Id);
         }
+
+        [TestMethod]
+        public async Task AddEmptyPerson()
+        {
+            var service = new PersonService(new InMemoryRepository<Person>());
+            var result = await service.Add(null);
+
+            Assert.IsFalse(result.Successful);
+        }
+
+        [TestMethod]
+        public async Task AddExtraOldPerson()
+        {
+            var person = new Person { DateOfBirth = DateOnly.FromDateTime(DateTime.UtcNow.AddYears(200)), FirstName = "Juan", LastName = "Perez" };
+
+            var service = new PersonService(new InMemoryRepository<Person>());
+            var result = await service.Add(person);
+
+            Assert.IsFalse(result.Successful);
+        }
     }
 }
